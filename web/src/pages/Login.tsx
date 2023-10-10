3c5232eee35450548e77131ef18efb71196d9e2e
Saurav Ghosh <saurav.ghosh378@gmail.com>
2023-10-11 00:21:53 +0600

    register and login screen design implemented

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,5 +1,85 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return <div>Login</div>;
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(formData);
+    };
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-[#202124]">
+            <form
+                onSubmit={handleSubmit}
+                className="w-[500px] rounded bg-[#525355] p-6 shadow-md"
+            >
+                <div className="mb-4">
+                    <label
+                        className="mb-2 block text-sm font-bold text-white"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="mb-2 block text-sm font-bold text-white"
+                        htmlFor="password"
+                    >
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        type="submit"
+                        className="focus:shadow-outline rounded bg-[#e67e22] px-4 py-2 font-bold text-white hover:bg-[#d35400] focus:outline-none"
+                    >
+                        Login
+                    </button>
+                    <Link
+                        to="/register"
+                        className="inline-block align-baseline text-sm font-bold text-white hover:text-[#e67e22]"
+                    >
+                        Not registered?
+                    </Link>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Login;
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,5 +1,104 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-    return <div>Register</div>;
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(formData);
+    };
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-[#202124]">
+            <form
+                onSubmit={handleSubmit}
+                className="w-[500px] rounded bg-[#525355] p-6 shadow-md"
+            >
+                <div className="mb-4">
+                    <label
+                        className="mb-2 block text-sm font-bold text-white"
+                        htmlFor="username"
+                    >
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                        className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                        placeholder="Username"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="mb-2 block text-sm font-bold text-white"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="mb-2 block text-sm font-bold text-white"
+                        htmlFor="password"
+                    >
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        type="submit"
+                        className="focus:shadow-outline rounded bg-[#e67e22] px-4 py-2 font-bold text-white hover:bg-[#d35400] focus:outline-none"
+                    >
+                        Register
+                    </button>
+                    <Link
+                        to="/login"
+                        className="inline-block align-baseline text-sm font-bold text-white hover:text-[#e67e22]"
+                    >
+                        Already registered?
+                    </Link>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Register;
