e64d35edf81f83e5fb86be0c86dc689ac67a7a6f
Saurav Ghosh <saurav.ghosh378@gmail.com>
2023-10-14 01:35:41 +0600

    parse zod validation errors

--- api/src/middlewares/validate.ts
+++ api/src/middlewares/validate.ts
@@ -3,9 +3,10 @@ import type { Context, Next } from "koa";
 import { z } from "zod";
 import { UnknownError, ValidationError } from "@base/errors";
 import { TSchema } from "@base/types";
+import eh from "@base/error_handler";
 
 const validate = <T, T2>(schema: TSchema) => {
-    return (ctx: Context, next: Next) => {
+    return eh((ctx: Context, next: Next) => {
         try {
             if (schema.body) {
                 ctx.request.body = schema.body.parse(ctx.request.body) as T2;
@@ -16,12 +17,12 @@ const validate = <T, T2>(schema: TSchema) => {
             return next();
         } catch (e: any) {
             if (e instanceof z.ZodError) {
-                throw new ValidationError(e.message);
+                throw new ValidationError("Payload is invalid", e.message);
             } else {
                 throw new UnknownError(e?.message || "");
             }
         }
-    };
+    });
 };
 
 export default validate;
